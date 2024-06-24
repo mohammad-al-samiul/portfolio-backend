248e98cc61b4e64ee30d1dc40b6f185fa08e3464
Mohammad Al Samiul <alsamiul123@gmail.com>
2024-06-24 12:44:13 +0600

    database config completed

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,31 @@
 import app from "./app";
 import mongoose from "mongoose";
 import config from "./app/config";
+import { Server } from "http";
+
+let server: Server;
 
 async function main() {
-  await mongoose.connect(config.db_url as string);
-
-  app.listen(config.port, () => {
-    console.log(`Example app listening on port ${config.port}`);
-  });
+  try {
+    await mongoose.connect(config.db_url as string);
+    server = app.listen(config.port, () => {
+      console.log(`Server listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
-main().catch((err) => console.log(err));
+main();
+
+process.on("unhandledRejection", () => {
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on("uncaughtException", () => {
+  process.exit(1);
+});
