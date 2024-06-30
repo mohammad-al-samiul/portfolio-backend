d85fd53abe60a485032cb121d08dddce453ec88b
Mohammad Al Samiul <alsamiul123@gmail.com>
2024-06-30 22:03:44 +0600

    access token partial completed

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,6 +3,8 @@ import AppError from "../../errors/AppError";
 import { TLoginUser, TUser } from "./auth.interface";
 import { User } from "./auth.model";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import config from "../../config";
 
 const createUserIntoDB = async (payload: TUser) => {
   const result = await User.create(payload);
@@ -27,6 +29,16 @@ const loginUserFromDB = async (payload: TLoginUser) => {
   if (!isPasswordMatch) {
     throw new AppError(httpStatus.UNAUTHORIZED, "Password is incorrect!");
   }
+
+  //generate access token
+  const jwtPayload = {
+    email: isUserExist?.email,
+    role: isUserExist?.role,
+  };
+
+  const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret, {
+    expiresIn: "10d",
+  });
 };
 
 export const UserServices = {
