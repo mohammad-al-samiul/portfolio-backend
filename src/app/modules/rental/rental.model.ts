05b30d81f3b1816c3a3347f9eb866ea94283e090
Mohammad Al Samiul <alsamiul123@gmail.com>
2024-08-15 14:09:18 +0600

    rental model pre save function completed

--- src/app/modules/rental/rental.model.ts
+++ src/app/modules/rental/rental.model.ts
@@ -1,5 +1,9 @@
 import mongoose, { Schema } from "mongoose";
 import { TRental } from "./rental.interface";
+import { Bike } from "../bikes/bike.model";
+import AppError from "../../errors/AppError";
+import httpStatus from "http-status";
+import { User } from "../auth/auth.model";
 
 const rentalSchema = new Schema<TRental>({
   userId: {
@@ -25,6 +29,25 @@ const rentalSchema = new Schema<TRental>({
     type: Boolean,
     default: false,
   },
+});
+
+rentalSchema.pre("save", async function () {
+  const isBikeExist = await Bike.findOne({ _id: this.bikeId });
+  if (!isBikeExist) {
+    throw new AppError(httpStatus.NOT_FOUND, "Bike not found!");
+  }
+
+  if (!isBikeExist.isAvailable) {
+    throw new AppError(
+      httpStatus.SERVICE_UNAVAILABLE,
+      "Bike is not available!"
+    );
+  }
+
+  const isUserExist = await User.findOne({ _id: this.userId });
+  if (!isUserExist) {
+    throw new AppError(httpStatus.NOT_FOUND, "User is not found!");
+  }
 });
 
 export const Rental = mongoose.model<TRental>("Rental", rentalSchema);
